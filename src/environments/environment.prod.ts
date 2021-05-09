3d94ed672eff26235b2d1f8f9ce8d1813c73a8f5
Dennis Chacko <dennis.v.chacko@gmail.com>
2021-05-09 05:48:53 -0500

    GitHub Pages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  authO: {
+    issuer: 'https://dev-r1e4o7ny.us.auth0.com/',
+    clientId: 'uBqZpyhO9dqVa1gBSPeysuzmKNPChgTq',
+    redirectUri: 'http://localhost:4200/home',
+    scope: 'openid profile email',
+    response_type: 'id_token token',
+    logoutUrl:
+      'https://dev-r1e4o7ny.us.auth0.com/v2/logout?federated&returnTo=http://localhost:4200/landing&client_id=uBqZpyhO9dqVa1gBSPeysuzmKNPChgTq',
+    postLogoutRedirectUri: 'http://localhost:4200/landing',
+  },
 };
