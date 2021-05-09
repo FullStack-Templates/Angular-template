db38056224d14e7334cc7849e323d3d9ab5cae3c
Dennis Chacko <dennis.v.chacko@gmail.com>
2021-05-09 06:28:21 -0500

    fix production properties

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,11 +3,11 @@ export const environment = {
   authO: {
     issuer: 'https://dev-r1e4o7ny.us.auth0.com/',
     clientId: 'uBqZpyhO9dqVa1gBSPeysuzmKNPChgTq',
-    redirectUri: 'http://localhost:4200/home',
+    redirectUri: 'https://fullstack-templates.github.io/Angular-template/home',
     scope: 'openid profile email',
     response_type: 'id_token token',
     logoutUrl:
-      'https://dev-r1e4o7ny.us.auth0.com/v2/logout?federated&returnTo=http://localhost:4200/landing&client_id=uBqZpyhO9dqVa1gBSPeysuzmKNPChgTq',
-    postLogoutRedirectUri: 'http://localhost:4200/landing',
+      'https://dev-r1e4o7ny.us.auth0.com/v2/logout?federated&returnTo=https://fullstack-templates.github.io/Angular-template/landing&client_id=uBqZpyhO9dqVa1gBSPeysuzmKNPChgTq',
+    postLogoutRedirectUri: 'https://fullstack-templates.github.io/Angular-template/landing',
   },
 };
