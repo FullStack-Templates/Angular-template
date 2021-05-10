6450feeb6e4349f7af77ea6186341d82c76b37ec
Dennis Chacko <39276940+dvchacko@users.noreply.github.com>
2021-05-10 16:48:59 -0500

    Add unit test cases
    * add auth service related unit test cases
    
    * unit test cases - continued

--- src/app/auth/auth-service.service.spec.ts
+++ src/app/auth/auth-service.service.spec.ts
@@ -1,18 +1,77 @@
-import { TestBed } from '@angular/core/testing';
 
+import { TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { OAuthService } from 'angular-oauth2-oidc';
 import { AuthService } from './auth-service.service';
+jest.mock('angular-oauth2-oidc');
 
 describe('AuthServiceService', () => {
   let service: AuthService;
+  
+  let oauthService = new OAuthService(null,null,null,null,null,null,null,null,null);
 
   beforeEach(() => {
-    //TestBed.configureTestingModule({});
-    //service = TestBed.inject(AuthService);
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule.withRoutes([])
+      ],
+      declarations: [],
+      providers: [{
+        provide: AuthService,
+        useValue: {}
+      }]
+    }).compileComponents();
+    let router = TestBed.get(Router);
+    service = new AuthService(router, oauthService);
   });
 
   it('should be created', () => {
-    //expect(service).toBeTruthy();
-    expect(true).toBe(true);
+    expect(service).toBeTruthy();
+  });
+
+  it('Should logout', () => {
+    try {
+      service.logout();
+    }
+    catch {
+      fail("It should not reach here");
+    }
+  });
+
+  it('Should login', () => {
+    try {
+      jest.spyOn(oauthService, "loadDiscoveryDocumentAndLogin").mockImplementation(() => {
+        let result = new Promise<boolean>((resolve:any, reject: any) => {
+          resolve(true);
+        });
+        return result;
+      });
+      service.login();
+    }
+    catch {
+      fail("It should not reach here");
+    }
+  });
+
+  it('IsAuthenticated should return true', () => {
+    jest.spyOn(oauthService, "hasValidAccessToken").mockImplementation(() => {
+        return true;
+      });
+    jest.spyOn(oauthService, "hasValidIdToken").mockImplementation(() => {
+        return true;
+    });
+    expect(service.isAuthenticated()).toBe(true);
+  });
+
+  it('IsAuthenticated should return false', () => {
+    jest.spyOn(oauthService, "hasValidAccessToken").mockImplementation(() => {
+        return false;
+      });
+    jest.spyOn(oauthService, "hasValidIdToken").mockImplementation(() => {
+        return false;
+    });
+    expect(service.isAuthenticated()).toBe(false);
   });
 });
 
