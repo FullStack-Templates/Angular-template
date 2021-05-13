0de5a93397b5041d2a943f7686638e10990bede9
Dennis Chacko <dennis.v.chacko@gmail.com>
2021-05-13 09:12:47 -0500

    upgrade to Angular 12

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -5,12 +5,12 @@ describe('workspace-project App', () => {
   let page: AppPage;
 
   beforeEach(() => {
-    console.log("before each");
+    console.log('before each');
     page = new AppPage();
   });
 
   it('should display welcome message', () => {
-     console.log("before navigateTo");
+    console.log('before navigateTo');
     page.navigateTo();
     expect(page.getTitleText()).toEqual('angular-oidc-app app is running!');
   });
